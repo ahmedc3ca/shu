88b64bdccf254925a1b116ad694f5c52834a26a2
Ahmed Ezzo <ahmed.ezzo@epfl.ch>
2025-02-23 19:27:43 +0100

    ex1

--- src/main.cpp
+++ src/main.cpp
@@ -26,14 +26,15 @@ int main()
 {
 
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
+    -1.0f, -0.5f, 0.0f,
+     -0.5f,  0.5f, 0.0f,
+     0.0f, -0.5f, 0.0f,
+     0.5f,  0.5f, 0.0f,
+     1.0f, -0.5f, 0.0f,
     };
     unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+        0, 1, 2,   // first triangle
+        2, 3, 4    // second triangle
     };
 
 
