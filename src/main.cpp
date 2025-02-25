e458a0989ab59f6fa2fd1686e17df83926972872
Ahmed Ezzo <ahmed.ezzo@epfl.ch>
2025-02-25 19:40:00 +0100

    interpolated coordinates from shader

--- src/main.cpp
+++ src/main.cpp
@@ -56,14 +56,15 @@ int main()
         return -1;
     }
 
-    Shader ourShader("../shaders/default.vs", "../shaders/greenUniform.fs");
+    Shader ourShader("../shaders/inter.vs", "../shaders/inter.fs");
+
 
 
     float vertices[] = {
-        // positions         
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
     };
 
     //setup buffer objects
@@ -75,9 +76,12 @@ int main()
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     
 
     //set background color
